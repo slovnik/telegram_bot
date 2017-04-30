805841cc4c3a38038a9079f1332b4fad3ef0e863
Roman Peshkov <peshkovroman@gmail.com>
2017-04-30 23:05:30 +0200

    Slight refactor. Bot can run in polling mode as well as webhooks

--- main.go
+++ main.go
@@ -2,21 +2,38 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"os"
 
 	"github.com/slovnik/slovnik"
 
-	"net/http"
-
 	"gopkg.in/telegram-bot-api.v4"
+)
+
+const (
+	// EnvBotID contains environment variable name with Bot ID received from Botfather
+	EnvBotID = "SLOVNIK_BOT_ID"
+
+	// EnvAPIURL contains
+	EnvAPIURL = "SLOVNIK_API_URL"
+
+	// EnvWebhookURL contains
+	EnvWebhookURL = "SLOVNIK_WEBHOOK_URL"
 )
 
 func main() {
 
-	botID := os.Getenv("SLOVNIK_BOT_ID")
-	slovnikURL := os.Getenv("SLOVNIK_API_URL")
-	webhookURL := os.Getenv("SLOVNIK_WEBHOOK_URL")
+	botID, ok := os.LookupEnv(EnvBotID)
+	if !ok {
+		log.Panic(EnvBotID + " is not set!")
+	}
+
+	slovnikURL, ok := os.LookupEnv(EnvAPIURL)
+
+	if !ok {
+		log.Panic(EnvAPIURL + " is not set!")
+	}
 
 	var bot *tgbotapi.BotAPI
 	var err error
@@ -28,13 +45,29 @@ func main() {
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	webHook := tgbotapi.NewWebhook(webhookURL + "/bot" + bot.Token)
-	if _, err = bot.SetWebhook(webHook); err != nil {
-		log.Panic(err)
-	}
+	webhookURL, ok := os.LookupEnv(EnvWebhookURL)
 
-	updates := bot.ListenForWebhook("/bot" + bot.Token)
-	go http.ListenAndServe("0.0.0.0:8080", nil)
+	var updates <-chan tgbotapi.Update
+
+	if !ok {
+		log.Printf("%s environment variable is not set. Using polling.", EnvWebhookURL)
+
+		u := tgbotapi.NewUpdate(0)
+		u.Timeout = 60
+
+		updates, err = bot.GetUpdatesChan(u)
+
+	} else {
+		log.Printf("%s is set to '%s'. Using webhooks", EnvWebhookURL, webhookURL)
+
+		webHook := tgbotapi.NewWebhook(webhookURL + "/bot" + bot.Token)
+		if _, err = bot.SetWebhook(webHook); err != nil {
+			log.Panic(err)
+		}
+
+		updates = bot.ListenForWebhook("/bot" + bot.Token)
+		go http.ListenAndServe("0.0.0.0:8080", nil)
+	}
 
 	for update := range updates {
 		if update.Message == nil {
@@ -45,7 +78,11 @@ func main() {
 
 		messageText := ""
 
-		c, _ := slovnik.NewClient(slovnikURL)
+		c, err := slovnik.NewClient(slovnikURL)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
 		w, err := c.Translate(update.Message.Text)
 
 		if err != nil || len(w.Word) <= 0 {
@@ -57,7 +94,6 @@ func main() {
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, messageText)
 
 		msg.ParseMode = tgbotapi.ModeMarkdown
-		msg.ReplyToMessageID = update.Message.MessageID
 
 		bot.Send(msg)
 	}
